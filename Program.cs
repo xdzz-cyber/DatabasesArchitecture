39a070f1083e1e33a51fe6fe1032cacaf34b2d80
xdzz-cyber <55808715+xdzz-cyber@users.noreply.github.com>
2022-05-22 20:51:02 +0300

    Add files via upload
    Tenth lab

--- Program.cs
+++ Program.cs
@@ -1,63 +1,65 @@
-using Microsoft.Extensions.Configuration;
+﻿using DatabaseArchitectureTenthLab.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
-using MongoDB.Driver;
-using MongoDbForDbArchitecture.Models;
-
-IConfigurationBuilder builder = new ConfigurationBuilder()
-    .SetBasePath(AppContext.BaseDirectory)
-    .AddJsonFile("appsettings.json");
-var config = builder.Build();
-
-string? connectionString = config.GetConnectionString("DefaultConnection");
-MongoClient client = new MongoClient(connectionString);
-IMongoCollection<BsonDocument> supervisors = client.GetDatabase("TEST").GetCollection<BsonDocument>("supervisors");
-
-GetDatabaseNames(client).GetAwaiter();
-Console.ReadLine();
-
-GetCollections(client).GetAwaiter();
-Console.ReadLine();
-
-var supervisorsList = supervisors.Find(new BsonDocument()).ToList();
-
-SuperVisor sp = BsonSerializer.Deserialize<SuperVisor>(supervisorsList.First());
-BsonDocument spBson = sp.ToBsonDocument();
-
-Console.WriteLine(sp.ToJson());
-Console.WriteLine(spBson);
+using MongoDB.Bson.Serialization.Conventions;
 
 
-static async Task GetDatabaseNames(MongoClient client)
+BsonClassMap.RegisterClassMap<SuperVisor>(x =>
 {
-    using (var cursor = await client.ListDatabasesAsync())
-    {
-        var databaseDocuments = await cursor.ToListAsync();
-        foreach (var databaseDocument in databaseDocuments)
-        {
-            Console.WriteLine(databaseDocument["name"]);
-        }
-    }
-}
+    x.AutoMap(); 
+    x.MapMember(y => y.Name).SetElementName("name"); 
+});
 
-static async Task GetCollections(MongoClient client)
+
+var superVisor = new SuperVisor()
 {
-    using (var cursor = await client.ListDatabasesAsync())
-    {
-        var dbs = await cursor.ToListAsync();
-        foreach (var db in dbs)
-        {
-            Console.WriteLine("DB {0} has next collections:", db["name"]);
-            IMongoDatabase database = client.GetDatabase(db["name"].ToString());
-            using (var collCursor = await database.ListCollectionsAsync())
-            {
-                var colls = await collCursor.ToListAsync();
-                foreach (var col in colls)
-                {
-                    Console.WriteLine(col["name"]);
-                }
-            }
-            Console.WriteLine();
-        }
-    }
-}
+    Id = Guid.NewGuid(),
+    Age = 12,
+    Department = "Program Systems and Technologies",
+    Name = "Andrew Hoku",
+    LeadingSubject = "Math",
+    MiddleName = "Jojef"
+};
+
+Console.WriteLine(superVisor.ToBsonDocument());
+
+var superVisorSecond = new SuperVisor()
+{
+    Id = Guid.NewGuid(),
+    Age = 13,
+    Department = "Some department",
+    Name = "John Levit",
+    LeadingSubject = "Chemistry",
+    MiddleName = "Kayet"
+};
+
+Console.WriteLine(superVisorSecond.ToJson());
+
+
+var superVisorThird = new SuperVisor()
+{
+    Id = Guid.NewGuid(),
+    Age = 23,
+    Department = "ThirdDepartment",
+    Name = "John Malkovic",
+    LeadingSubject = "English",
+    MiddleName = "Fayout"
+};
+
+Console.WriteLine(superVisorThird.ToBsonDocument());
+
+var conventionPack = new ConventionPack();
+conventionPack.Add(new CamelCaseElementNameConvention());
+ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+var superVisorFourth = new SuperVisor()
+{
+    Id = Guid.NewGuid(),
+    Age = 23,
+    Department = "FourhtDepartment",
+    Name = "Jazzy Jazzy",
+    LeadingSubject = "Biology",
+    MiddleName = "Faust"
+};
+
+Console.WriteLine(superVisorFourth.ToBsonDocument());
